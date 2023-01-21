d0acd1e5a09c5d858beb3eb080f6bbcdbc63f291
Makeavoy <auxnon@gmail.com>
2023-01-21 02:29:33 -0500

    misc: config

--- config.lua
+++ config.lua
@@ -1,12 +1,17 @@
 dev = true
 alias = {
+    a = "load apps/logo",
     md = "attr{modernize=1}",
     crt = "attr{modernize=0}",
+    c = "load games/card",
+    l = "load test/concurrency",
     m = "load games/mini",
     q = "load games/witch",
     e = "load test/entbench",
     n = "load test/sound",
     b = "load test/bot",
     r = "load test/raster",
+    p = "load games/brawnpong",
+    g = "load games/bip",
     em = "load test/emodel",
 }
