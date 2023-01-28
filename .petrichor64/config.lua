ed9c2f40d00ca336acc2f947e1ea60b96e94fae1
Makeavoy <makeavoy@gmail.com>
2023-01-28 17:09:21 -0500

    fix: moved config to new debug output

---
+++ .petrichor64/config.lua
@@ -0,0 +1,18 @@
+dev = true
+alias = {
+    a = "load apps/logo",
+    b = "load apps/b",
+    md = "attr{modernize=1}",
+    crt = "attr{modernize=0}",
+    c = "load games/card",
+    l = "load test/concurrency",
+    m = "load games/maze",
+    q = "load games/witch",
+    e = "load test/entbench",
+    n = "load test/sound",
+    -- b = "load test/bot",
+    r = "load test/raster",
+    p = "load games/brawnpong",
+    g = "load games/bip",
+    em = "load test/emodel",
+}
