9a9958c8c81066d7fca51270fb8eb314c9d1bce5
Makeavoy <auxnon@gmail.com>
2023-04-30 23:32:59 -0400

    chore: guide bulding script

---
+++ build-tools/guide-builder.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const docHash = {};
+fs.readdirSync("./guide")
+  .filter((f) => f.endsWith(".md") && f !== "main.md")
+  .forEach((f) => {
+    const name = f.slice(0, -3);
+    docHash[name] = f;
+  });
+
+const main = fs.readFileSync("./guide/main.md", "utf8");
+let output = "";
+const lines = main.split("\n");
+lines.forEach((line) => {
+  if (line.startsWith("### ")) {
+    const name = line.slice(4).trim();
+    const docName = docHash[name];
+    if (docName) {
+      const doc = fs.readFileSync(`./guide/${docName}`, "utf8");
+      output += doc + "\n---\n";
+      delete docHash[name];
+    } else {
+      throw new Error(`⛔️ Missing required doc for ${name}`);
+    }
+  } else {
+    output += line + "\n";
+  }
+});
+
+const missing = Object.keys(docHash);
+if (missing.length) {
+  throw new Error(
+    `⛔️ Commands we found no documents for, do they exist?: ${missing.join(
+      ", "
+    )}`
+  );
+} else {
+  console.log("✅ guide built");
+}
+fs.writeFileSync("./dist/guide.md", output, "utf8");
